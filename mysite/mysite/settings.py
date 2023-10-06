c88411d7f72b828d57332497bd47003ec5eb2817
Majid <m.asari1375@gmail.com>
2023-10-06 17:41:17 +0330

    fix: fix bug in celery broker url

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -126,7 +126,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # region celery
-CELERY_BROKER_URL = "redis://127.0.0.1:6379/0"
+CELERY_BROKER_URL = "redis://redis:6379/0"
 CELERY_ACCEPT_CONTENT = ["application/json"]
 CELERY_TASK_SERIALIZER = "json"
 CELERY_TASK_ALWAYS_EAGER = False
