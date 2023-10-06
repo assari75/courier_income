fc7e1bce159d5253edbcb4a2b6e7c37f7e701892
Majid <m.asari1375@gmail.com>
2023-10-06 13:19:38 +0330

    fix: use transaction atomic in create or update daily income records

--- mysite/courier_income/signals.py
+++ mysite/courier_income/signals.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from django.db.models import signals
 from django.dispatch import receiver
 
@@ -8,4 +9,5 @@ from . import models
 @receiver(signals.post_save, sender=models.Reward)
 @receiver(signals.post_save, sender=models.Forfeit)
 def create_or_update_daily_income(sender, instance, **kwargs):
-    instance.create_or_update_daily_income()
+    with transaction.atomic():
+        instance.create_or_update_daily_income()
